da13567b55470c22d42a76f773a3863fed065837
Samirolivier <samirolivier@gmail.com>
2024-09-22 18:49:14 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,10 @@
 <?php
-$host = 'localhost';
-$dbname = 'zooarcadia';
-$username = 'root';
-$password = ''; 
+$cleardb_url = parse_url("mysql://bf5c8f7d96a88e:64840b04@eu-cluster-west-01.k8>
+
+$host = $cleardb_url["host"];
+$username = $cleardb_url["user"];
+$password = $cleardb_url["pass"];
+$database = substr($cleardb_url["path"], 1);
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
